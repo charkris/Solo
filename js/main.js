cb7f59e6e8999f9a729ceaa3ee9fad19f7fa36b4
Kristina Charkviani <kcharkviani@bog.ge>
2022-09-01 20:29:16 +0400

    markup added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
-import { Home } from "./modules/home.js"
+import { Home } from "./modules/home.js";
 
-let home = new Home()
+let home = new Home();
+
+console.log("ტესტ");
+
+let myApi = 'https://mocki.io/v1/be185936-09b7-4a9d-a1d6-cfab28941003'
+
+//'https://mocki.io/v1/a17138de-3164-48ab-b7dc-1468aecf2bd4'
+//big = https://mocki.io/v1/be185936-09b7-4a9d-a1d6-cfab28941003
+
+// myApi = JSON.stringify(myApi);
+
+// fetch('https://solo.ge/api/developers/items/common/608aaad7ae3b47ff23daa433?fromParam=2000&toParam=2500&skip=0&limit=9').then(response => response.json())
+//   .then(data => console.log(data));
+
+
+//   fetch('https://mocki.io/v1/a17138de-3164-48ab-b7dc-1468aecf2bd4') 
+//   .then((resp) => resp.json())
+//   .then((res) => {
+//     console.log(res.data.items);
+//   });
